b3e5e8d860c5e78e71dd4467dd33569fc2890bde
Azumabashi <>
2023-08-07 14:22:52 +0900

    redefine type PropLogicFormula to switch the property of the object with formula type

--- src/propositionalLogic.nim
+++ src/propositionalLogic.nim
@@ -1,28 +1,43 @@
 type
   PropFormulaType* {.pure.} = enum
     atomicProp, andProp, orProp, notProp, impliesProp
-  PropLogicFormula* = ref object of RootObj
-  AtomicProp* = ref object of PropLogicFormula
-    id*: int
-  AndFormula* = ref object of PropLogicFormula
-    left*, right*: PropLogicFormula
-  OrFormula* = ref object of PropLogicFormula
-    left*, right*: PropLogicFormula
-  NotFormula* = ref object of PropLogicFormula
-    formula*: PropLogicFormula
-  ImpliesFormula* = ref object of PropLogicFormula
-    antecedent*, consequent*: PropLogicFormula
+  PropLogicFormula* = ref object
+    # cf. https://github.com/momeemt/minim/blob/main/src/minim/asts.nim#L16-L47
+    case formulaType*: PropFormulaType
+    of PropFormulaType.atomicProp:
+      id*: int
+    of PropFormulaType.andProp, PropFormulaType.orProp:
+      left*, right*: PropLogicFormula
+    of PropFormulaType.notProp:
+      formula*: PropLogicFormula
+    of PropFormulaType.impliesProp:
+      antecedent*, consequent*: PropLogicFormula
   TruthVaue* = ref object
     value*: bool
 
-proc `&`* (left, right: PropLogicFormula): AndFormula = 
-  AndFormula(left: left, right: right)
+proc `&`* (left, right: PropLogicFormula): PropLogicFormula = 
+  PropLogicFormula(
+    formulaType: PropFormulaType.andProp,
+    left: left,
+    right: right
+  )
 
-proc `|`*(left, right: PropLogicFormula): OrFormula = 
-  OrFormula(left: left, right: right)
+proc `|`*(left, right: PropLogicFormula): PropLogicFormula = 
+  PropLogicFormula(
+    formulaType: PropFormulaType.orProp,
+    left: left,
+    right: right
+  )
 
-proc `!`*(formula: PropLogicFormula): NotFormula =
-  NotFormula(formula: formula)
+proc `!`*(formula: PropLogicFormula): PropLogicFormula =
+  PropLogicFormula(
+    formulaType: PropFormulaType.notProp,
+    formula: formula
+  )
 
-proc `=>`*(antecedent, consequent: PropLogicFormula): ImpliesFormula =
-  ImpliesFormula(antecedent: antecedent, consequent: consequent)
+proc `=>`*(antecedent, consequent: PropLogicFormula): PropLogicFormula =
+  PropLogicFormula(
+    formulaType: PropFormulaType.impliesProp,
+    antecedent: antecedent,
+    consequent: consequent
+  )
