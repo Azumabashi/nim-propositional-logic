222eb43e08e9d05dabc8aad7669742abf4b4ff26
Azumabashi <>
2023-08-21 14:29:40 +0900

    [feat] define proc formulaToInterpretationTypeSeq

--- src/propositionalLogic/simplification.nim
+++ src/propositionalLogic/simplification.nim
@@ -1,3 +1,21 @@
+import formulae
+import interpretationUtils
+import evalUtils
+import truthValue
+import tables
+
 type
   InterpretationType {.pure.} = enum
     top, bot, dontCare
+
+proc formulaToInterpretationTypeSeq(formula: PropLogicFormula): seq[seq[InterpretationType]] =
+  let numberOfAtomicProps = getNumberOfAtomicProps()
+  for interpretation in interpretations():
+    if formula.isSat(interpretation):
+      var interpretationTypeSeq: seq[InterpretationType] = @[]
+      for id in 1..numberOfAtomicProps:
+        if interpretation[id] == TOP:
+          interpretationTypeSeq.add(InterpretationType.top)
+        else:
+          interpretationTypeSeq.add(InterpretationType.bot)
+      result.add(interpretationTypeSeq)
