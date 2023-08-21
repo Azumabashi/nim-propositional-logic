ceebf754cc89a7ba14a70055a6641c33acde6123
Azumabashi <>
2023-08-21 14:37:18 +0900

    change return type

--- src/propositionalLogic/simplification.nim
+++ src/propositionalLogic/simplification.nim
@@ -8,14 +8,20 @@ type
   InterpretationType {.pure.} = enum
     top, bot, dontCare
 
-proc formulaToInterpretationTypeSeq(formula: PropLogicFormula): seq[seq[InterpretationType]] =
+proc formulaToInterpretationTypeSeq(formula: PropLogicFormula): Table[int, seq[seq[InterpretationType]]] =
   let numberOfAtomicProps = getNumberOfAtomicProps()
   for interpretation in interpretations():
     if formula.isSat(interpretation):
-      var interpretationTypeSeq: seq[InterpretationType] = @[]
+      var 
+        interpretationTypeSeq: seq[InterpretationType] = @[]
+        numberOfTop = 0
       for id in 1..numberOfAtomicProps:
         if interpretation[id] == TOP:
           interpretationTypeSeq.add(InterpretationType.top)
+          numberOfTop += 1
         else:
           interpretationTypeSeq.add(InterpretationType.bot)
-      result.add(interpretationTypeSeq)
+      if result.hasKey(numberOfTop):
+        result[numberOfTop].add(interpretationTypeSeq)
+      else:
+        result[numberOfTop] = @[interpretationTypeSeq]
