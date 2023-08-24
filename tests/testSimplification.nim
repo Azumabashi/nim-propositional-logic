353240e5c7ffaa7adb32fdc4163d5204fecb0510
Azumabashi <>
2023-08-24 23:00:36 +0900

    initialize just once

--- tests/testSimplification.nim
+++ tests/testSimplification.nim
@@ -5,15 +5,14 @@ import propositionalLogic
 suite "make logical formula simple":
   # test case is taken from https://en.wikipedia.org/wiki/Quine%E2%80%93McCluskey_algorithm
   # (as of 2023/08/21)
-  setup:
-    let 
-      A = generateAtomicProp()
-      B = generateAtomicProp()
-      C = generateAtomicProp()
-      D = generateAtomicProp()
+  let 
+    A = generateAtomicProp()
+    B = generateAtomicProp()
+    C = generateAtomicProp()
+    D = generateAtomicProp()
+  
+  test "checking logical equivalence":
     let
       formula = (!A & B & !C & !D) | (A & !B & !C & !D) | (A & !B & C & !D) | (A & !B & C & D) | (A & B & !C & !D)  | (A & B & C & D)
       simplerFormula = formula.simplification()
-  
-  test "checking logical equivalence":
     check (formula => simplerFormula).isTautology() and (simplerFormula => formula).isTautology()
