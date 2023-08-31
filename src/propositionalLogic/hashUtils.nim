06b7bec6fce56d408055aa1fe337f7b1b48ec1c3
Azumabashi <>
2023-08-31 13:58:10 +0900

    [feat] support hashing for PropLogicFormula

---
+++ src/propositionalLogic/hashUtils.nim
@@ -0,0 +1,16 @@
+## This module provides proc `hash` for `PropLogicFormula` (the algorithm of 
+## proc `hash` this module provides can be improved).
+
+import hashes
+import formulae
+
+proc hash*(formula: PropLogicFormula): Hash =
+  ## Returns hash value of given `formula`.
+  !$ recByStructure(
+    formula,
+    proc (formula: PropLogicFormula): Hash = hash(formula.id),
+    proc (left, right: Hash): Hash = (left and right).Hash,
+    proc (left, right: Hash): Hash = (left or right).Hash,
+    proc (antecedent, consequent: Hash): Hash = ((not antecedent) or consequent).Hash,
+    proc (val: Hash): Hash = (not val).Hash
+  )
