126d906d902b35b926346e0a81c2982c262fbae9
Azumabashi <>
2023-08-31 14:29:06 +0900

    export proc numberOfInterpretations

--- src/propositionalLogic/interpretationUtils.nim
+++ src/propositionalLogic/interpretationUtils.nim
@@ -8,7 +8,7 @@ type
     ## Type alias represents interpretation.
     ## The key is id of an atomic proposition.
 
-proc numberOfInterpretations(atomicProps: int): int = 1 shl atomicProps
+proc numberOfInterpretations*(atomicProps: int): int = 1 shl atomicProps
 
 iterator interpretations*(): Interpretation =
   ## Iterator generates all interpretations to all atomic propositions.
