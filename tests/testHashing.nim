2ba150d8803158cefd52f93cfc504ff4cdd59363
Azumabashi <>
2023-09-03 16:49:55 +0900

    add test for hashing of TruthValue

--- tests/testHashing.nim
+++ tests/testHashing.nim
@@ -5,7 +5,7 @@ import sets
 
 import propositionalLogic
 
-suite "test for hashing":
+suite "test for formula hashing":
   let
     P = generateAtomicProp()
     Q = generateAtomicProp()
@@ -30,3 +30,21 @@ suite "test for hashing":
     check keys.contains(formula1)
     check keys.contains(formula2)
     check not keys.contains(formula3)
+
+suite "test for truth value hashing":
+  let t = {TOP: 1, BOTTOM: 0}.toTable
+
+  test "get keys":
+    let keys = t.keys().toSeq()
+    check keys.len == 2
+    check keys.contains(TOP)
+    check keys.contains(BOTTOM)
+  
+  test "get values":
+    check t[TOP] == 1
+    check t[BOTTOM] == 0
+  
+  test "test for hash sets":
+    let s = [TOP, BOTTOM].toHashSet()
+    check s.contains(TOP)
+    check s.contains(BOTTOM)
