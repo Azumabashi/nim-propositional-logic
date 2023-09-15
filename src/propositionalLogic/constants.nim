1509587b49f48f893609a88a37dd0ce34727e3ae
Azumabashi <>
2023-09-15 22:05:50 +0900

    [feat] define proc isOperator

--- src/propositionalLogic/constants.nim
+++ src/propositionalLogic/constants.nim
@@ -5,3 +5,8 @@ const
   orSymbol* = "|"
   notSymbol* = "!"
   impliesSymbol* = "=>"
+
+proc isOperator*(x: string): bool =
+  ## Returns `true` is `x` is a string corresponds to one of the operators, and
+  ## `false` otherwise.
+  x == andSymbol or x == orSymbol or x == notSymbol or x == impliesSymbol
