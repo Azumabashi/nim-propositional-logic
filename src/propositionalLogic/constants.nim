c035fe95b04715ef7b6301fbbf0761af95e72450
Azumabashi <>
2023-09-15 22:08:31 +0900

    add comment for documentation

--- src/propositionalLogic/constants.nim
+++ src/propositionalLogic/constants.nim
@@ -1,10 +1,10 @@
 const
-  leftParen* = "("
-  rightParen* = ")"
-  andSymbol* = "&"
-  orSymbol* = "|"
-  notSymbol* = "!"
-  impliesSymbol* = "=>"
+  leftParen* = "("  ## String corresponds to left paren.
+  rightParen* = ")" ## String corresponds to right paren.
+  andSymbol* = "&"  ## String corresponds to logical connective AND.
+  orSymbol* = "|"   ## String corresponds to logical connective OR.
+  notSymbol* = "!"  ## String corresponds to logical connective NOT.
+  impliesSymbol* = "=>"  ## String corresponds to logical connective IMPLIES.
 
 proc isOperator*(x: string): bool =
   ## Returns `true` is `x` is a string corresponds to one of the operators, and
