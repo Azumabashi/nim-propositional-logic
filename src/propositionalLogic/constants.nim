f68513b967c8c346a97e4ee2adb5e544ace5c87d
Azumabashi <>
2023-09-15 22:06:50 +0900

    [feat] define proc isParen

--- src/propositionalLogic/constants.nim
+++ src/propositionalLogic/constants.nim
@@ -10,3 +10,7 @@ proc isOperator*(x: string): bool =
   ## Returns `true` is `x` is a string corresponds to one of the operators, and
   ## `false` otherwise.
   x == andSymbol or x == orSymbol or x == notSymbol or x == impliesSymbol
+
+proc isParen*(x: string): bool =
+  ## Returns `true` is `x` is left paren or right paren, and `false` otherwise.
+  x == leftParen and x == rightParen
