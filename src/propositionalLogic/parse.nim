488a9dbf9ff8300dc658045dadf1de52b590b56c
Azumabashi <>
2023-09-17 14:24:10 +0900

    [feat] define proc parse

--- src/propositionalLogic/parse.nim
+++ src/propositionalLogic/parse.nim
@@ -1,4 +1,8 @@
 import constants
+import formulae
+import deques
+import strutils
+import algorithm
 
 proc toReversePolishNotation(formula: string): seq[string] =
   var 
@@ -12,20 +16,59 @@ proc toReversePolishNotation(formula: string): seq[string] =
       level += 1
     elif token == rightParen:
       level -= 1
-    elif token.isOperator():
+    elif token.isOperator() or token == "=":
       if token == "=":
         i += 1
         assert formula[i] == '>'
         token = "=>"
       if operatorLevelPairs.len > 0 and operatorLevelPairs[^1][1] > level:
-        for operatorLevelPair in operatorLevelPairs:
+        for operatorLevelPair in operatorLevelPairs.reversed:
           result.add(operatorLevelPair[0])
         operatorLevelPairs = @[]
       operatorLevelPairs.add((token, level))
     else:
-      var j = 1
-      while not isOperator($(formula[i+j])) and not isParen($(formula[i+j])):
+      var j = i + 1
+      while j < formula.len and not (
+        formula[j] == '=' or isOperator($(formula[j])) or isParen($(formula[j]))
+      ):
         j += 1
-      result.add(formula[i..<i+j])
-  for operatorLevelPair in operatorLevelPairs:
+      result.add(formula[i..<j].strip())
+      i = j - 1
+    i += 1
+  
+  for operatorLevelPair in operatorLevelPairs.reversed:
     result.add(operatorLevelPair[0])
+
+proc parse*(formula: string): PropLogicFormula =
+  let reversePolishNotation = formula.toReversePolishNotation()
+  var deque = initDeque[PropLogicFormula]()
+  for token in reversePolishNotation:
+    if token.isOperator():
+      case token
+      of andSymbol:
+        let
+          right = deque.popLast()
+          left = deque.popLast()
+        deque.addLast(left & right)
+      of orSymbol:
+        let
+          right = deque.popLast()
+          left = deque.popLast()
+        deque.addLast(left | right)
+      of notSymbol:
+        let subFormula = deque.popLast()
+        deque.addLast(!subFormula)
+      of impliesSymbol:
+        let
+          consequent = deque.popLast()
+          antecedent = deque.popLast()
+        deque.addLast(antecedent => consequent)
+      else:
+        assert false  # This cannot be reached!
+    elif not token.isParen():
+      let id = token.parseInt()
+      deque.addLast(generateAtomicPropWithGivenId(id))
+    else:
+      assert false
+  assert deque.len == 1
+  result = deque.popLast()
